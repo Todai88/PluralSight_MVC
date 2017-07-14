67ba2b6258674e81d506fa797047a5ce83b19183
U-JABERO05\joaki <joabaj88@gmail.com>
2017-07-14 11:31:00 +0100

    Finished Scaffolding

---
+++ EntitiyFramework/Books/Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Books.Entities;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public BooksDb() 
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
