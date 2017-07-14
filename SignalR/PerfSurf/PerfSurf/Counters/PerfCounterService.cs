4b35ba3f2032e051ef7c7026debab2aaeea4828b
U-JABERO05\joaki <joabaj88@gmail.com>
2017-07-14 14:17:59 +0100

    Finished SignalR

---
+++ SignalR/PerfSurf/PerfSurf/Counters/PerfCounterService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfSurf.Counters
+{
+    public class PerfCounterService
+    {
+        List<PerfCounterWrapper> _counters;
+
+        public PerfCounterService()
+        {
+            _counters = new List<PerfCounterWrapper>();
+
+            _counters.Add(new PerfCounterWrapper("Processor",
+                "Processor", "% Processor Time", "_Total"));
+
+            _counters.Add(new PerfCounterWrapper("Paging",
+                "Memory", "Pages/sec"));
+
+            _counters.Add(new PerfCounterWrapper("Disk",
+                "PhysicalDisk", "% Disk Time", "_Total"));
+        }
+
+        public dynamic GetResults()
+        {
+            return _counters.Select(c => new
+            { name = c.Name, value = c.Value });
+        }
+    }
+}
---
+++ SignalR/PerfSurf/PerfSurf/Counters/PerfCounterWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfSurf.Counters
+{
+    public class PerfCounterWrapper
+    {
+        public string Name { get; set; }
+        public float Value
+        {
+            get
+            {
+                return _counter.NextValue();
+            }
+        }
+        PerformanceCounter _counter;
+
+        public PerfCounterWrapper(string name, string category,
+                                  string counter, string instance = "")
+        {
+            _counter = new PerformanceCounter(category, counter, instance, readOnly: true);
+            Name = name;
+        }
+    }
+}
